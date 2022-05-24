3d98df8987d61593401ebd86136bd72171f8e4f3
Crystian Lefundes <crystian.lf@gmail.com>
2022-05-23 22:15:27 -0300

    Create Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,7 @@
+import { Image } from './styles'
+
+export const Logo = () => {
+  return(
+    <Image src='/assets/images/logo.png' alt='' />
+  )
+}
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from "styles/theme";
+
+
+
+export const Image = styled('img', {
+  maxWidth: 100,
+  maxHeight: 100
+})
